a248a07eb3df046c5b8747e25773fea32edb7e06
Hapopa <82116969+Hapopa@users.noreply.github.com>
2023-10-19 22:32:47 +0300

    add appbar and background of app

--- lib/Layout/home.dart
+++ lib/Layout/home.dart
@@ -4,6 +4,21 @@ class HomeScreen extends StatelessWidget {
 static const String routreName='homeScreen';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/images/pattern.png'),
+        fit: BoxFit.cover
+        ),
+      ),
+    child: Scaffold(
+      appBar: AppBar(
+        title: Text('Palatine News'),
+        shape: OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent),
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20))
+        ),
+      ),
+      backgroundColor: Colors.transparent,
+    ),
+    );
   }
 }
